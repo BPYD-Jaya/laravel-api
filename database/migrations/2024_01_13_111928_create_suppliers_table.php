dc221923b314ec97806b391aad4c9ceb28c62a07
Naufal Randika <naufalrandika44@gmail.com>
2024-01-13 18:30:31 +0700

    Initial commit & migration

---
+++ database/migrations/2024_01_13_111928_create_suppliers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('company_name');
+            $table->string('product');
+            $table->string('price');
+            $table->integer('stock');
+            $table->string('unit');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('location');
+            $table->string('image');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
