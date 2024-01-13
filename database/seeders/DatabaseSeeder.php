d0c60b97b3e60743bcec49a157d211a82a7951d1
Naufal Randika <naufalrandika44@gmail.com>
2024-01-13 19:00:10 +0700

    migrations & user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Uncomment the following line if you want to use the User factory.
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+        ]);
+    }
+}
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the seeder.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            $hashedPassword = Hash::make('P@ssw0rd');
+
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@ptwpi.co.id',
+                'password' => $hashedPassword,
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]);
+
+            echo 'Seeding successful: User created' . PHP_EOL;
+        } catch (\Exception $e) {
+            echo 'Seeding failed: ' . $e->getMessage() . PHP_EOL;
+        }
     }
 }
